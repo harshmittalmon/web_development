170c7d8c9479967bcdc79a0d502d7b6a338b6517
chetna <chetnamittal010@gmail.com>
2023-11-16 17:26:23 +0530

    updated recipe project ui

---
+++ recipes/index.js
@@ -0,0 +1,12 @@
+let var1 = document.querySelector('#recipe-list');
+var1.style.cssText = "background-color:white;";
+let var2 = document.querySelector('#recepi-list1')  ;
+var2.textContent = "hi text content is successfooool";
+const content = document.createElement('div');
+content.classList.add(aisehi);
+const container  = document.querySelector('#recepi-list1');
+
+container.appendChild(content);
+const btn  = document.querySelector('button');
+console.log(btn);
+btn.classList.add('btn');
