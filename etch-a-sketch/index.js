64900d0f2ba6dd2b3e08e56d9eeff896c12905eb
chetna <chetnamittal010@gmail.com>
2023-12-11 20:19:45 +0530

    basic portfolio created

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,43 +1,44 @@
-function makegrid(container,num){
-    for(let i = 0;i<num;i++){
+
+function makegrid(container, num) {
+    for (let i = 0; i < num; i++) {
         const div = document.createElement('div');
 
         let prop = getComputedStyle(container);
         let width = 1000;
-        let k=1 ;
-        for(k=1;k<100;k++){
-            if( (num*k) >500)break;
+        let k = 1;
+        for (k = 1; k < 100; k++) {
+            if ((num * k + 2 * num) > (600)) break;
         }
-        let height=k-1;
-        console.log(height);
-        div.setAttribute('style','display:flex;');
+        let height = k - 1;
+        div.setAttribute('style', 'display:flex;');
         div.classList.add('second-level');
-        for(let j=0;j<num;j++){
+        for (let j = 0; j < num; j++) {
             const divchild = document.createElement('div');
             // divchild.textContent = `i is ${i} and j is ${j}`;
-            divchild.style.width = ""+width+"px";
-            console.log(height);
-            divchild.style.height = ""+height+"px";     
+            divchild.style.width = "" + width + "px";
+            divchild.style.height = "" + height + "px";
             divchild.classList.add('third-level');
             div.appendChild(divchild);
+
         }
         container.appendChild(div);
-    } 
+    }
 }
-function submitForm(event) {
-    // Prevent the default form submission
-    event.preventDefault();
-    
-    // Get the value from the input element
-     num = document.querySelector('input').value;
-
-    // Check if the user entered something
-    
-}
+var isdouble = false;
 let maindiv = document.createElement('div');
 maindiv.classList.add('maindiv');
 let body = document.querySelector('body');
 body.appendChild(maindiv);
 let num = prompt("enter the grid size : ");
 
-makegrid(maindiv,num);
+makegrid(maindiv, num);
+let nodelist = document.querySelectorAll('.third-level');
+nodelist.forEach(element => {
+    element.addEventListener('dblclick', function (e) {
+        isdouble = true;
+        e.target.style.backgroundColor = "white";
+        console.log(e.target);
+    })
+});
+
+
